744419c8ef3de57809979e8bb688b46580ec2194
DonMahallem <donmahallem@users.noreply.github.com>
2019-01-21 18:12:35 +0100

    fix dumb errors

--- src/flow-api.ts
+++ src/flow-api.ts
@@ -13,84 +13,31 @@ export class FlowApi {
     private readonly baseUrl: string = "https://flow.polar.com";
 
 
-    public getJar2(): request.CookieJar {
+    public getJar(): request.CookieJar {
         return this.cookieJar2;
     }
-    public signin3(): Promise<request.Response> {
-        return new Promise((resolve, reject) => {
-            request.get("https://flow.polar.com/login", {
-                jar: this.cookieJar2,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-                }
-            }, function (err, httpResponse, body) {
-                console.log("JJJJ");
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(httpResponse);
-                }
-            })
-        });
-    }
-    public signin2(): Promise<request.Response> {
 
+    /**
+     * 
+     * @param mail The mail used to login to flow.polar.com
+     * @param password the password used to login to flow.polar.com
+     */
+    public signin(mail: string, password: string): Promise<request.Response> {
         const data: any = {
-            email: Config.email,
-            password: Config.password,
-            returnUrl: "/"
-        };/*
-        console.log("Request data", querystring.stringify(data));
-        return Axios.post("https://flow.polar.com/login", querystring.stringify(data), {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Origin': 'https://flow.polar.com',
-                'Referer': 'https://flow.polar.com/login',
-                'Content-Length': querystring.stringify(data).length,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-            },
-            maxRedirects: 1,
-            transformRequest: this.createCookieInserter(this.cookieJar),
-            transformResponse: this.createCookieCatcher(this.cookieJar)
-        });*/
+            returnUrl: "/",
+            email: mail,
+            password: password
+        };
         return new Promise((resolve, reject) => {
-            console.log("JJJJ");
-            requestDebug(request, function (type, data, r) {
-                // put your request or response handling logic here
-                if (type === "response") {
-                    console.log("response", data.headers);
-                } else if (type === "request") {
-                    console.log("request", data);
-                } else if (type === "redirect") {
-                    console.log("redirect");
-                } else if (type === "auth") {
-                    console.log("auth");
-                } else {
-                    console.log("other");
-                }
-            });
-            request.post({
-                url: 'https://flow.polar.com/login',
+            request.post('https://flow.polar.com/login', {
                 form: data,
-                method: "post",
-                strictSSL: true,
-                host: "flow.polar.com",
                 jar: this.cookieJar2,
-                gzip: false,
                 headers: {
-                    'accept-language': 'en-US,en;q=0.9,de;q=0.8',
-                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                    'cookie': 'NSC_eob1-qspe-gmpx-mc=ffffffffc3a0947a45525d5f4f58455e445a4a4229a1; timezone=60',
-                    'dnt': '1',
-                    'x-requested-with': 'XMLHttpRequest',
-                    'Origin': 'https://flow.polar.com',
-                    'Referer': 'https://flow.polar.com/',
-                    'Content-Length': querystring.stringify(data).length,
-                    'accept': 'application/json',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+                    //'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    //'content-length': querystring.stringify(data).length,
+                    'user-agent': 'Mozilla/5.0'
                 }
             }, function (err, httpResponse, body) {
-                console.log("JJJJ");
                 if (err) {
                     reject(err);
                 } else {
@@ -102,4 +49,35 @@ export class FlowApi {
     }
 
 
+    public getActivityTimelineForDay(year: number, month: number, day: number, sampleCount: number = 50000): Promise<request.Response> {
+        if (month < 1 || month > 12) {
+            return Promise.reject(new Error("The month must be equal to or between 1 and 12"));
+        }
+        if (day < 1 || day > 31) {
+            return Promise.reject(new Error("The day must be equal to or between 1 and 31"));
+        }
+        if (sampleCount < 1) {
+            return Promise.reject(new Error("Samplecount must be atleast 1"));
+        }
+        const _year: string = "" + year;
+        const _month: string = (month < 10) ? ("0" + month) : ("" + month);
+        const _day: string = (day < 10) ? ("0" + day) : ("" + day);
+        return new Promise((resolve, reject) => {
+            request.get('https://flow.polar.com/api/activity-timeline/load?day=' + _year + '-' + _month + '-' + _day + '&maxSampleCount=' + sampleCount, {
+                jar: this.cookieJar2,
+                headers: {
+                    //'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    //'content-length': querystring.stringify(data).length,
+                    'user-agent': 'Mozilla/5.0'
+                }
+            }, function (err, httpResponse, body) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(httpResponse);
+                }
+            })
+        });
+
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { FlowApi } from "./flow-api";
+import { Config } from "./config";
 
 const a: FlowApi = new FlowApi();
-a.signin2()
+
+console.log(process.argv);
+a.signin(Config.email, Config.password)
+    .then((res) => {
+        return a.getActivityTimelineForDay(2019, 1, 18);
+    })
     .then((res) => {
         console.log("Res", res.statusCode, res.headers);
     })
