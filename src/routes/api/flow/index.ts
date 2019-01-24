1f6b6f4c8c777e6471d620d0938367eae72f8cf8
DonMahallem <donmahallem@users.noreply.github.com>
2019-01-24 10:21:54 +0100

    savasdf

--- src/routes/api/flow/index.ts
+++ src/routes/api/flow/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { FlowApiClient } from '@donmahallem/flowapi';
 import { IConfig } from '../../../config';
+import { Response } from 'request';
 const flowApiClient: FlowApiClient = new FlowApiClient();
 
 export const rhGetActivityTimeLineForDay: express.RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -8,7 +9,8 @@ export const rhGetActivityTimeLineForDay: express.RequestHandler = (req: express
         .then((dataRes) => {
             res.send(dataRes.body);
         })
-        .catch((err) => {
+        .catch((err: Error | Response) => {
+            console.log(err);
             res.status(400).send("error");
         })
 };
@@ -16,8 +18,13 @@ export const rhGetActivityTimeLineForDay: express.RequestHandler = (req: express
 
 export const createFlowApiRoute = (config: IConfig): express.Router => {
     const apiRoute: express.Router = express.Router();
+    flowApiClient.signin(config.flow.email, config.flow.password);
+    apiRoute.use((req, res, next) => {
+        console.log(req.path);
+        next();
+    })
     apiRoute.get("/activity/timeline/:date(\d{4,4}\-\d{2,2}\-\d{2,2})", rhGetActivityTimeLineForDay);
-    apiRoute.get("/sleep/nearby/:date(\d{4,4}\-\d{2,2}\-\d{2,2})", rhGetActivityTimeLineForDay);
-    apiRoute.get("/sleep/:id(\d+)", rhGetActivityTimeLineForDay);
+    apiRoute.get("/sleep/nearby/:date(\\d{4,4}\\-\\d{2,2}\\-\\d{2,2})", rhGetActivityTimeLineForDay);
+    apiRoute.get("/sleep/:id(\\d+)", rhGetActivityTimeLineForDay);
     return apiRoute;
 }
